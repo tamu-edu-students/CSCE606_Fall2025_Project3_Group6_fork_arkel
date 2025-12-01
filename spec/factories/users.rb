ea0780b3bc397d33b86bef1443f20d7326549bf5
Evergreen0929 <1425962132@qq.com>
2025-12-01 11:03:25 -0600

    Fix lint

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     username { "user_#{Faker::Number.number(digits: 5)}" }
-    password { "Password123!" } 
+    password { "Password123!" }
     password_confirmation { "Password123!" }
     confirmed_at { Time.current }
   end
